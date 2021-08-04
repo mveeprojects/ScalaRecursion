0a83086a41d7ddb937401e1e4621e263e4a53420
mva13 <markvallis1987@Gmail.com>
2021-08-04 22:55:23 +0100

    Updating example amd README.md

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Simple examples of recursion and how to convert them to tail recursion in Scala
 
 ### Sources:
 * https://alvinalexander.com/scala/fp-book/recursion-thinking-recursively-function-signatures/
+* https://alvinalexander.com/scala/fp-book/recursion-jvm-stacks-stack-frames/
+* https://alvinalexander.com/scala/fp-book/recursion-visual-look-jvm-stack-frames/
 * https://alvinalexander.com/scala/fp-book/tail-recursive-algorithms/
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,41 @@
+import scala.annotation.tailrec
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val listOfNumbers = List.range(1, 4) // List(1, 2, 3, Nil)
+    println(sum(listOfNumbers))
+    println(sumTailRecursive(listOfNumbers, 0))
+  }
+
+  /*
+*** Thread output of sum() ***
+Main$.sum(Main.scala:12)
+Main$.sum(Main.scala:14)
+Main$.sum(Main.scala:14)
+Main$.sum(Main.scala:14)
+Main$.main(Main.scala:6)
+Main.main(Main.scala)
+6
+   */
+  def sum(numberList: List[Int]): Int = numberList match {
+    case Nil =>
+      Thread.currentThread().getStackTrace.foreach(println)
+      0
+    case head :: tail => head + sum(tail)
+  }
+
+  /*
+*** Thread output of sumTailRecursive() ***
+Main$.sumTailRecursive(Main.scala:30)
+Main$.main(Main.scala:7)
+Main.main(Main.scala)
+6
+   */
+  @tailrec
+  def sumTailRecursive(numberList: List[Int], accumulator: Int): Int = numberList match {
+    case Nil =>
+      Thread.currentThread().getStackTrace.foreach(println)
+      accumulator
+    case head :: tail => sumTailRecursive(tail, head + accumulator)
+  }
+}
